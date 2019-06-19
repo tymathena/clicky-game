660b8115b00a25f61ad17d441a586aa5f8514aaf
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-06-19 10:09:53 -0400

    Updates

---
+++ precache-manifest.1871498b05e8eac3aab2b873e4eca62b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6975b3747e002c9e94913fba89ea39",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "74ef2f0a6d397517e7aa",
+    "url": "/clicky-game/static/css/main.608a7957.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "74ef2f0a6d397517e7aa",
+    "url": "/clicky-game/static/js/main.c5726eb7.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
